18598e0133f11853278f6898d8eabefe43c68bc8
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-05-01 22:14:25 +0700

    tes: minify js

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ const cases = require("./database/Schema/Case")
 const { json, urlencoded } = require("body-parser")
 const { resolve } = require("path")
 const { minify } = require('html-minifier');
+const JavaScriptObfuscator = require('javascript-obfuscator');
 
 app.set("view engine", "ejs");
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(express.static(resolve(__dirname, "views")));
 app.use(express.static(resolve(__dirname, "public")));
+
+// MENGACAK SCRIPT DI HTML
+function obfuscateInlineScripts(html) {
+    return html.replace(/<script>([\s\S]*?)<\/script>/g, (match, jsCode) => {
+      const obfuscated = JavaScriptObfuscator.obfuscate(jsCode, {
+        compact: true,
+        controlFlowFlattening: true,
+        deadCodeInjection: true,
+        stringArray: true,
+        rotateStringArray: true,
+      }).getObfuscatedCode();
+  
+      return `<script>${obfuscated}</script>`;
+    });
+  }
 
 /* app.get("/", (req, res) => {
     res.sendStatus(200)
@@ -25,11 +41,13 @@ app.use(express.static(resolve(__dirname, "public")));
 app.get('/', (req, res) => {
     res.render('index', { bot: client, req, res }, (err, html) => {
       if (err) return res.status(500).send(err.message);
+
+      const obfuscatedHTML = obfuscateInlineScripts(html);
   
-      const minifiedHtml = minify(html, {
+      const minifiedHtml = minify(obfuscatedHTML, {
         collapseWhitespace: true,
         removeComments: true,
-        minifyJS: true,
+        //minifyJS: true,
         minifyCSS: true,
         ignoreCustomFragments: [/<%[\s\S]*?%>/]  // agar tag EJS tidak rusak
       });

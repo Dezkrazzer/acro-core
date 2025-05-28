6641bbce4b2d20c8ba69877ef2a2ab4c3779a3e5
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-05-28 22:12:18 +0700

    feat(store): adding page store

--- server.js
+++ server.js
@@ -56,6 +56,24 @@ app.get('/', (req, res) => {
     });
   });
 
+app.get('/store', (req, res) => {
+    res.render('store', { bot: client, req, res }, (err, html) => {
+      if (err) return res.status(500).send(err.message);
+
+      const obfuscatedHTML = obfuscateInlineScripts(html);
+  
+      const minifiedHtml = minify(obfuscatedHTML, {
+        collapseWhitespace: true,
+        removeComments: true,
+        //minifyJS: true,
+        minifyCSS: true,
+        ignoreCustomFragments: [/<%[\s\S]*?%>/]  // agar tag EJS tidak rusak
+      });
+  
+      res.send(minifiedHtml);
+    });
+});
+
 app.get("/api/case", async (req, res) => {
     try {
         const data = await cases.find({});

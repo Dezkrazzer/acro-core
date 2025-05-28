82ed784d35146fdbb1daab7134cecc4ac56a9c85
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-05-28 23:30:44 +0700

    chore(avatar): added can search with globalname

--- commands/Misc/avatar.js
+++ commands/Misc/avatar.js
@@ -6,7 +6,18 @@ module.exports = {
     description: "View other people's profile photos in HD quality",
     category: "Misc",
     run: async (client, message, args) => {
-        let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+        let user =
+            message.mentions.users.first() ||
+            client.users.cache.get(args[0]) ||
+            // Cari berdasarkan username (case-insensitive)
+            (args[0]
+                ? client.users.cache.find(
+                      u =>
+                          u.username.toLowerCase() === args.join(" ").toLowerCase() ||
+                          (u.globalName && u.globalName.toLowerCase() === args.join(" ").toLowerCase())
+                  )
+                : null) ||
+            message.author;
         let image = user.avatarURL({ dynamic: true, size: 4096 });
 
         let EmbedAvatar = new Discord.EmbedBuilder()
